5862bf21104bf935669a73c9eb1e5e331fb28d43
pranay dhomne <88877415+pranaydhomne@users.noreply.github.com>
2023-06-19 16:55:06 +0530

    static files collected

--- project1/settings.py
+++ project1/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-#a3*9fspvb1#24+#u^*(6rx*5n_lju8bqwu&8v4iy@-p)uamj6
 DEBUG = False #for deployment
 # DEBUG = True #for localhot
 
-ALLOWED_HOSTS = ["*pip "]
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -117,9 +117,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATICFILES_DIRS = os.path.join(BASE_DIR,'static'),
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
