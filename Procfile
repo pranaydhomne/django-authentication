a911c4f4fa5d83dbd09c6a442178aceb785c4a63
pranay dhomne <88877415+pranaydhomne@users.noreply.github.com>
2023-06-19 15:32:16 +0530

    all file added

---
+++ Procfile
@@ -0,0 +1,1 @@
+web : gunicorn PROJECT1.wsgi  --log.file -
--- project1/settings.py
+++ project1/settings.py
@@ -23,9 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-#a3*9fspvb1#24+#u^*(6rx*5n_lju8bqwu&8v4iy@-p)uamj6'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False #for deployment
+# DEBUG = True #for localhot
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*pip "]
 
 
 # Application definition
